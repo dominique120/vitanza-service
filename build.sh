c4121774c7d6356118b47d9bac771fef8fa5b0c2
Dominique <dominique120@users.noreply.github.com>
2020-10-26 00:07:44 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,21 +1,23 @@
 apt-get install -y git cmake build-essential libmariadb-dev-compat libboost-system-dev libcurl4-openssl-dev libssl-dev uuid-dev zlib1g-dev libpulse-dev 
 
 
-git clone https://github.com/aws/aws-sdk-cpp.git
-cd aws-sdk-cpp
-mkdir build 
-cd build
-cmake .. -DBUILD_ONLY="dynamodb" -DENABLE_TESTING=OFF -DCUSTOM_MEMORY_MANAGEMENT=OFF  -DBUILD_SHARED_LIBS=OFF
-make
-make install
 
-cd ..
-cd ..
+git clone https://github.com/aws/aws-sdk-cpp.git
+mkdir aws-sdk-cpp.build && cd aws-sdk-cpp.build
+cmake ../aws-cpp-sdk -DBUILD_ONLY="dynamodb" -DENABLE_TESTING=OFF -DCUSTOM_MEMORY_MANAGEMENT=OFF  -DBUILD_SHARED_LIBS=OFF
+make && make install
+
 
 git clone https://github.com/meltwater/served.git
-cd served 
-mkdir build 
-cd build
-cmake .. -DSERVED_BUILD_EXAMPLES=OFF -DSERVED_BUILD_TESTS=OFF
+mkdir served.build && cd served.build
+cmake ../served -DSERVED_BUILD_EXAMPLES=OFF -DSERVED_BUILD_TESTS=OFF -DSERVED_BUILD_SHARED=ON -DSERVED_BUILD_STATIC=ON
+make && make install
+ln /usr/local/lib/libserved.so.1.4 /usr/lib64/libserved.so.1.4
+
+
+
+clone Vitanza Service
+mkdir vts.build && cd vts.build
+cmake ../vts
 make
-make install
+
