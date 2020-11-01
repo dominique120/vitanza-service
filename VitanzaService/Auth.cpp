cd340398ddfc025e9576c6df7f0329ccec1e71df
Dominique <dominique120@users.noreply.github.com>
2020-10-31 23:34:40 -0500

    FIxing travis compile issues

--- VitanzaService/Auth.cpp
+++ VitanzaService/Auth.cpp
@@ -2,5 +2,13 @@
 #include "Auth.h"
 
 std::string Auth::hash_password(const std::string& plain_password, const std::string& salt) {
-	return std::string();
+	return "";
+}
+
+std::string Auth::generate_token(const std::string& username, const std::string& password) {
+	return "";
+}
+
+bool Auth::validate_token(const std::string& token) {
+	return true;
 }
--- VitanzaService/auth_wrapper.cpp
+++ VitanzaService/auth_wrapper.cpp
@@ -1,2 +1,10 @@
 ﻿#include "vtspch.h"
 #include "auth_wrapper.h"
+
+bool auth_wrapper::save_new_user(const std::string& username, const std::string& password) {
+	return true;
+}
+
+bool auth_wrapper::authenticate(const std::string& username, const std::string& password) {
+	return true;
+}
