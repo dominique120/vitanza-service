e405c030d4108a62749f51cf0b11d5a69d6fd0b3
Dominique <dominique120@users.noreply.github.com>
2020-11-08 13:24:15 -0500

    Update main.h

--- VitanzaService/main.h
+++ VitanzaService/main.h
@@ -41,6 +41,5 @@
 
 int main(int argc, char* argv[]);
 void register_handlers(served::multiplexer& mux);
-bool start_server(served::multiplexer& mux, const int& threads, const bool& block);
 
 #endif
